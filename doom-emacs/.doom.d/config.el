ba3030699d3acfc6b287cba4ae83bc54430219b5
guy <guydovlev@gmail.com>
2022-04-24 20:19:34 +0300

    backup

---
+++ doom-emacs/.doom.d/config.el
@@ -0,0 +1,9 @@
+(setq user-full-name "Guy"
+      user-mail-address "guydovlev@protonmail.com")
+
+(setq doom-theme 'doom-nord)
+
+(setq display-line-numbers-type t)
+
+(setq org-directory "~/org/")
+(setq doom-font (font-spec :family "JetBrains Mono Nerd Font" :size 15))
